30e8af56f62df1cb6ab3eca179560b0264ee9df9
DESKTOP-HQ3992N\Oswin <toni.bgae@gmail.com>
2021-03-22 13:51:40 +0100

    Basic Spawn Manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    GameObject[] spawnPoints;
+    [SerializeField]
+    GameObject[] enemiesPrefabs;
+
+
+    [SerializeField]
+    float timeBetweenWaves;
+    float timeUntilNextWave;
+
+    int nextEnemy;
     void Start()
     {
-        
+        timeUntilNextWave = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timeUntilNextWave <=0)
+        {
+            for(int i = 0; i < spawnPoints.Length; i++)
+            {
+                nextEnemy = Random.Range(0, enemiesPrefabs.Length);
+                Instantiate(enemiesPrefabs[nextEnemy], spawnPoints[i].transform.position, enemiesPrefabs[nextEnemy].transform.rotation);
+            }
+            timeUntilNextWave = timeBetweenWaves;
+        }
+        else
+        {
+            timeUntilNextWave -= Time.deltaTime;
+        }
     }
 }
